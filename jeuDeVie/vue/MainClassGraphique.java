43a85d1223c8159e072c8dd1ed001e1a152c4f4d
Aurelien <aurel.andre@yahoo.fr>
2016-10-26 15:55:24 +0200

    Fin du code

---
+++ jeuDeVie/vue/MainClassGraphique.java
@@ -0,0 +1,41 @@
+package jeuDeVie.vue;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+import jeuDeVie.modele.AlgoGenetique;
+import jeuDeVie.modele.Modele;
+
+@SuppressWarnings("serial")
+public class MainClassGraphique extends JFrame{
+
+	public MainClassGraphique(){
+		
+		super("Systemes Complexes Adaptatifs - Jeu de vie");
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	    Modele m = new Modele();
+	    //AlgoGenetique ag = new AlgoGenetique(m);
+	    //m.affichageGrille();
+	    //ag.algo(150000000);
+	    
+	    VueGraphique vg = new VueGraphique(m);
+	    this.add(vg, BorderLayout.NORTH);
+	    
+	    VueBoutons vb = new VueBoutons(m);
+	    
+	   
+        this.add(vb, BorderLayout.CENTER);
+        this.setJMenuBar(new VueMenu(m));
+		
+        m.miseAJour();
+        
+        pack() ;
+        setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new MainClassGraphique();
+	}
+}
